36e8322562e814854f0223bc009f1be94ca5a59e
Ruslan Sharipov <ruslan.b.sharipov@gmail.com>
2018-11-24 16:22:45 +0300

    added javaDoc comments to the PersonByNameComparator class

--- src/main/java/repository/comparator/PersonByNameComparator.java
+++ src/main/java/repository/comparator/PersonByNameComparator.java
@@ -5,6 +5,15 @@ import model.Person;
 import java.util.Comparator;
 
 public class PersonByNameComparator implements Comparator<Person> {
+    /**
+     * Compares tho Person objects by their id
+     *
+     * @param o1 first object
+     * @param o2 second object
+     * @return positive int value if the name of the first person has to be closer to the beginning of the repository
+     * that name of the second person, 0 if the names are equal, else returns negative int value
+     * @throws NullPointerException if one or two of specified objects are null
+     */
     @Override
     public int compare(Person o1, Person o2) {
         if (o1 != null && o2 != null) {
