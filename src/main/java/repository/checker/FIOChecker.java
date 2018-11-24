94a36def5233fbaa67c878de1f07b86f37728bd2
Ruslan Sharipov <ruslan.b.sharipov@gmail.com>
2018-11-24 16:54:26 +0300

    added javaDoc comments to the FIOChecker class

--- src/main/java/repository/checker/FIOChecker.java
+++ src/main/java/repository/checker/FIOChecker.java
@@ -1,15 +1,27 @@
 package repository.checker;
 
 import model.Person;
-import repository.checker.Checker;
 
 public class FIOChecker implements Checker<Person> {
+    /**
+     * Query matching to which will be checked in check() method
+     */
     private String query;
 
+    /**
+     * Constructs FIOChecker object
+     *
+     * @param s {@link #query}
+     */
     public FIOChecker(String s) {
         this.query = s;
     }
 
+    /**
+     * Checks if the persons name contains {@link #query}
+     * @param person person to check
+     * @return <tt>true</tt> if person's name contains {@link #query}
+     */
     @Override
     public boolean check(Person person) {
         if (person != null && query != null) {
