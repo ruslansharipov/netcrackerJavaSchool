770162ecb987c1e941e0e29e5a6d9da6d99c2131
Ruslan Sharipov <ruslan.b.sharipov@gmail.com>
2018-11-24 16:37:55 +0300

    added javaDoc comments to the Person class

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -7,20 +7,37 @@ public class Person {
     private String name;
     private LocalDate birthDate;
 
+    /**
+     * Constructs Person object
+     *
+     * @param id        person's id
+     * @param name      person's name
+     * @param birthDate person's date of birth
+     */
     public Person(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
     }
 
+    /**
+     * @return person's id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * @return person's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Calculates the age of the person
+     * @return person's age in years
+     */
     public Integer getAge() {
         return LocalDate
                 .now()
@@ -28,10 +45,18 @@ public class Person {
                 .getYear();
     }
 
+    /**
+     * @return the person's birthday
+     */
     public LocalDate getBirthDay() {
         return birthDate;
     }
 
+    /**
+     * Checks the equality of this object with specified object
+     * @param obj object to check equality with
+     * @return <tt>true</tt> if objects are equal, else return false
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) return false;
