638888afa2edb683382a02d871c4f4014c1b5cc6
Ruslan Sharipov <ruslan.b.sharipov@gmail.com>
2018-11-24 16:10:51 +0300

    added javaDoc comments to the PersonByAgeComparator class

--- src/main/java/repository/comparator/PersonByAgeComparator.java
+++ src/main/java/repository/comparator/PersonByAgeComparator.java
@@ -5,6 +5,15 @@ import model.Person;
 import java.util.Comparator;
 
 public class PersonByAgeComparator implements Comparator<Person> {
+    /**
+     * Compares tho Person objects by their age
+     *
+     * @param o1 first object
+     * @param o2 second object
+     * @return positive int value if the age of the first person is more of that second person, 0 if the ages are equal,
+     * else returns negative int value
+     * @throws NullPointerException if one or two of specified objects are null
+     */
     @Override
     public int compare(Person o1, Person o2) {
         if (o1 != null && o2 != null) {
